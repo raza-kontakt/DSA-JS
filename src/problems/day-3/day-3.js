b3d8caa2adef0f7685a3c4694783c42db59c3b7d
Ali Raza <alirazamunir401@gmail.com>
2025-04-25 22:41:44 +0200

    day 3,  isPlaindrome

---
+++ src/problems/day-3/day-3.js
@@ -0,0 +1,57 @@
+// CHECK IF NUMBER IS PLAINDROME
+// Given an integer n, find whether the number is Palindrome or not. A number is a Palindrome if it remains the same when its digits are reversed.
+// Examples::
+// Input: n = 12321
+// Output: Yes
+// Explanation: 12321 is a Palindrome number because after reversing its digits, the number becomes 12321 which is the same as the original number.
+// Input: n = 1234
+// Output: No
+// Explanation: 1234 is not a Palindrome number because after reversing its digits, the number becomes 4321 which is different from the original number.
+function isPlaindrome(num) {
+    var input = num.toString();
+    for (var index = 0; index <= input.length / 2; index++) {
+        var start = input[index];
+        var end = input[input.length - 1 - index];
+        if (start !== end)
+            return false;
+    }
+    return true;
+}
+var testCases = [
+    {
+        input: 12321,
+        expected: true,
+    },
+    {
+        input: 1234,
+        expected: false,
+    },
+    {
+        input: 1, // Single digit
+        expected: true,
+    },
+    {
+        input: 11, // Two digits
+        expected: true,
+    },
+    {
+        input: 121, // Three digits
+        expected: true,
+    },
+    {
+        input: 12345, // Five digits
+        expected: false,
+    },
+];
+function testIsPlaindrome() {
+    testCases.map(function (item) {
+        var result = isPlaindrome(item.input);
+        if (result === item.expected) {
+            console.log("✅ Pass: For input " + item.input);
+        }
+        else {
+            console.log("❌ Fail: For input " + item.input);
+        }
+    });
+}
+testIsPlaindrome();
